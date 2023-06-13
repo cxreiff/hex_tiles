5b83046b2e1ab9fe561e3d41b58be3af91f0360e
cxreiff <cooper@cxreiff.com>
2023-06-12 20:34:54 -0700

    added height and color drag edit

--- src/camera_plugin.rs
+++ src/camera_plugin.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
+use bevy_mod_picking::prelude::*;
 
 pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
@@ -8,14 +10,17 @@ impl Plugin for CameraPlugin {
 }
 
 fn camera_setup(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 10.0, 15.0)
-            .looking_at(Vec3::new(0.0, 1.2, 0.0), Vec3::Y),
-        projection: Projection::Orthographic(OrthographicProjection {
-            scale: 4.,
-            scaling_mode: ScalingMode::FixedVertical(2.),
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 10.0, 15.0)
+                .looking_at(Vec3::new(0.0, 1.2, 0.0), Vec3::Y),
+            projection: Projection::Orthographic(OrthographicProjection {
+                scale: 4.,
+                scaling_mode: ScalingMode::FixedVertical(2.),
+                ..default()
+            }),
             ..default()
-        }),
-        ..default()
-    });
+        },
+        RaycastPickCamera::default(),
+    ));
 }
