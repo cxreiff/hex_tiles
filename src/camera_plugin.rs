ed864ba5a382302898dd293f85e11a2c66344df9
cxreiff <cooper@cxreiff.com>
2023-06-16 21:29:30 -0700

    tile placing added, color changing removed

--- src/camera_plugin.rs
+++ src/camera_plugin.rs
@@ -10,12 +10,21 @@ impl Plugin for CameraPlugin {
 }
 
 fn camera_setup(mut commands: Commands) {
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(-2.0, 15.0, 0.0),
+        point_light: PointLight {
+            intensity: 8000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        ..default()
+    });
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 10.0, 15.0)
-                .looking_at(Vec3::new(0.0, 1.2, 0.0), Vec3::Y),
+            transform: Transform::from_xyz(-4.0, 10.0, 12.0)
+                .looking_at(Vec3::new(0.0, 0.8, 0.0), Vec3::Y),
             projection: Projection::Orthographic(OrthographicProjection {
-                scale: 4.,
+                scale: 5.8,
                 scaling_mode: ScalingMode::FixedVertical(2.),
                 ..default()
             }),
