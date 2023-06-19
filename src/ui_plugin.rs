18bfef037d7460a028910722fb8102be7245dc8d
cxreiff <dev@cooperreiff.com>
2023-06-18 22:38:56 -0700

    added rudimentary UI setup

---
+++ src/ui_plugin.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+
+use crate::{loading_plugin::LoadedAssets, GameState};
+
+pub struct UiPlugin;
+impl Plugin for UiPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(ui_setup.in_schedule(OnEnter(GameState::Playing)))
+            .add_system(ui_control);
+    }
+}
+
+fn ui_setup(mut commands: Commands, assets: Res<LoadedAssets>) {
+    commands.spawn(NodeBundle {
+        style: Style {
+            size: Size::width(Val::Percent(100.0)),
+            justify_content: JustifyContent::SpaceBetween,
+            padding: UiRect::all(Val::Percent(2.0)),
+            ..default()
+        },
+        ..default()
+    })
+    .with_children(|parent| {
+        parent.spawn(
+            TextBundle::from_section(
+                "use arrow keys to orbit camera.\nclick to add tiles.",
+                TextStyle {
+                    font: assets.font.clone(),
+                    font_size: 20.,
+                    color: Color::PURPLE,
+                },
+            ),
+        );
+    });
+}
+
+fn ui_control(mut _commands: Commands) {
+    //
+}
