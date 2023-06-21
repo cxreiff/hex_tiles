45497c4f9ad82bb8a6cba4e1f7916ec4f70dad11
cxreiff <dev@cooperreiff.com>
2023-06-20 21:11:32 -0700

    added info icon to instructions

--- src/ui_plugin.rs
+++ src/ui_plugin.rs
@@ -16,20 +16,60 @@ fn ui_setup(mut commands: Commands, assets: Res<LoadedAssets>) {
             style: Style {
                 size: Size::width(Val::Percent(100.0)),
                 justify_content: JustifyContent::SpaceBetween,
-                padding: UiRect::all(Val::Percent(2.0)),
+                padding: UiRect::new(
+                    Val::Percent(3.0),
+                    Val::Percent(3.0),
+                    Val::Percent(2.0),
+                    Val::Percent(2.0),
+                ),
+                gap: Size::all(Val::Percent(2.0)),
                 ..default()
             },
             ..default()
         })
         .with_children(|parent| {
-            parent.spawn(TextBundle::from_section(
-                "use arrow keys to orbit camera.\nclick to add tiles.",
-                TextStyle {
-                    font: assets.font.clone(),
-                    font_size: 18.,
-                    color: Color::PURPLE,
-                },
-            ));
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        gap: Size::all(Val::Px(5.0)),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "",
+                        TextStyle {
+                            font: assets.font.clone(),
+                            font_size: 48.,
+                            color: Color::PURPLE,
+                        },
+                    ));
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                padding: UiRect::all(Val::Px(6.0)),
+                                ..default()
+                            },
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn(
+                                TextBundle::from_section(
+                                    "arrow keys to orbit camera.\nleft lick to add tiles.",
+                                    TextStyle {
+                                        font: assets.font.clone(),
+                                        font_size: 18.,
+                                        color: Color::PURPLE,
+                                    },
+                                )
+                                .with_style(Style {
+                                    padding: UiRect::all(Val::Percent(5.0)),
+                                    ..default()
+                                }),
+                            );
+                        });
+                });
         });
 }
 
